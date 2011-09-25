9368a27a700a9aa35ecdb4eeeb55d124001d7b2b
nakul <nakul.raut5@gmail.com>
2011-09-25 19:01:01 +0530

    removed extra files

--- runQuery/Release/runQuery.bat
+++ runQuery/Release/runQuery.bat
@@ -1,22 +1,15 @@
 
-@echo off
+REM @echo off
 call "D:\Softwares\Siemens\Teamcenter8\tc_menu\tc9_MYDB.bat"
-set start=%TIME%
+for /f "tokens=1-4 delims=:." %%a in ("%TIME%") do (
+    set /a start_total=%%a*3600 + %%b*60 + %%c
+)
+:: Run your command
+"D:\Projects\RunQuery\Release\RunQuery.exe"
+for /f "tokens=1-4 delims=:." %%a in ("%TIME%") do (
+    set /a end_total=%%a*3600 + %%b*60 + %%c
+)
+set /a elapsed=end_total-start_total
+echo Execution time: %elapsed% seconds
 
-:: Run your command here
-"D:\VS_WS\runQuery\Release\runQuery.exe"
-
-set end=%TIME%
-
-:: Extract hours, minutes, seconds, and milliseconds
-for /f "tokens=1-4 delims=:." %%a in ("%start%") do set start_h=%%a&set start_m=%%b&set start_s=%%c&set start_ms=%%d
-for /f "tokens=1-4 delims=:." %%a in ("%end%") do set end_h=%%a&set end_m=%%b&set end_s=%%c&set end_ms=%%d
-
-:: Calculate elapsed time
-set /a elapsed_h=end_h-start_h
-set /a elapsed_m=end_m-start_m
-set /a elapsed_s=end_s-start_s
-set /a elapsed_ms=end_ms-start_ms
-
-echo Execution time: %elapsed_h%:%elapsed_m%:%elapsed_s%.%elapsed_ms% seconds
 pause
--- runQuery/runQuery/runQuery.cpp
+++ runQuery/runQuery/runQuery.cpp
@@ -1,131 +1,134 @@
 //FIRST CODE
+#include "header.h"
+
+
+using namespace std;
+int ITK_user_main(int argc, char* argv[]){
+	printf("Utility started execution...\n");
+	int iFail = ITK_ok;
+	iFail = ITK_auto_login();
+	//iFail = ITK_init_module("infodba","infodba","dba");
+	if(iFail!=ITK_ok)
+	{
+		printf("Auto login unsuccessful\n");
+		return iFail;
+	}
+	printf("Auto login successful\n");
+	//FIND QUERY
+	tag_t tQryTag = NULLTAG;
+	utils::report_error(QRY_find("General...",&tQryTag));
+	if(tQryTag!=NULLTAG){
+		printf("Query found\n");
+		//CREATE QRY INPUTS
+		int tEntryCnt = 1;
+		char* cValue1 = "Item";
+		char* cEntries[] = {"Type"};
+		char** cValues = (char**)calloc(tEntryCnt,sizeof(char*));
+		if(!cValues){
+			printf("Memory allocation failed for cValues\n");
+			utils::report_error(1);
+			return 1;
+		}
+		cValues[0]=(char*)calloc(strlen(cValue1)+1,sizeof(char));
+		if(!cValues[0]){
+			printf("Memory allocation failed for cValues[0]\n");
+			MEM_free(cValues);
+			cValues=NULL;
+			utils::report_error(1);
+			return 1;
+		}
+		strcpy_s(cValues[0],strlen(cValue1) + 1,cValue1);
+		//RUN QUERY
+		int iCnt;
+		tag_t* tQryTags = NULL;
+		utils::report_error(QRY_execute(tQryTag,tEntryCnt,cEntries,cValues,&iCnt,&tQryTags));
+		printf("Query count 1= %d\n",iCnt);
+		for(int i=0;i<tEntryCnt;i++){
+			MEM_free(cValues[i]);
+		}
+		printf("Query count 2= %d\n",iCnt);
+		/*if( iCnt > 0 ){
+			printf("Query count 3= %d\n",iCnt);
+		}*/
+		if (tQryTags) {
+			MEM_free(tQryTags);
+		}
+		MEM_free(cValues);
+		
+	}else{
+		printf("Query not found\n");
+	}
+	ITK_exit_module(true);
+	return iFail;
+}
+
+
+//SECOND CODE	
 //#include "header.h"
 //#include <memory>
+//#include <cstring>
 //
 //struct MemDeleter {
 //	void operator()(tag_t* ptr) const {
 //		if (ptr) MEM_free(ptr);
 //	}
 //};
+//
 //using namespace std;
-//int ITK_user_main(int argc, char* argv[]){
+//
+//int ITK_user_main(int argc, char* argv[]) {
 //	printf("Utility started execution...\n");
 //	std::unique_ptr<tag_t, MemDeleter> tRawTags;
 //	int iFail = ITK_ok;
 //	iFail = ITK_auto_login();
-//	//iFail = ITK_init_module("infodba","infodba","dba");
-//	if(iFail!=ITK_ok)
-//	{
+//
+//	if (iFail != ITK_ok) {
 //		printf("Auto login unsuccessful\n");
 //		return iFail;
 //	}
 //	printf("Auto login successful\n");
 //	tag_t tQryTag = NULLTAG;
-//	utils::report_error(QRY_find("General...",&tQryTag));
-//	if(tQryTag!=NULLTAG){
+//	utils::report_error(QRY_find("General...", &tQryTag));
+//	if (tQryTag != NULLTAG) {
 //		printf("Query found\n");
 //		int tEntryCnt = 1;
-//		char* cValue1 = "Item";
-//		char* cEntries[] = {"Type"};
-//		char** cValues = (char**)malloc(tEntryCnt*sizeof(char*));
-//		if(!cValues){
+//		const char* cValue1 = "Item";
+//		 char* cEntries[] = {"Type"};
+//
+//		// Use smart pointer for dynamic array allocation
+//		unique_ptr<char*[]> cValues(new char*[tEntryCnt]);
+//		if (!cValues) {
 //			printf("Memory allocation failed for cValues\n");
 //			return 1;
 //		}
-//		
-//		cValues[0]=(char*)malloc(strlen(cValue1)+1);
-//		if(!cValues[0]){
+//
+//		// Allocate memory for the first entry
+//		cValues[0] = new char[strlen(cValue1) + 1];
+//		if (!cValues[0]) {
 //			printf("Memory allocation failed for cValues[0]\n");
-//			MEM_free(cValues);
-//			return 2;
+//			return 1;
 //		}
-//		strcpy_s(cValues[0],strlen(cValue1) + 1,cValue1);
-//		
+//
+//		// Copy string safely
+//		strcpy_s(cValues[0], strlen(cValue1) + 1, cValue1);
 //		int iCnt = 0;
-//		tag_t* tQryTags = NULL;
-//		// Smart pointer with custom deleter
-//		utils::report_error(QRY_execute(tQryTag,tEntryCnt,cEntries,cValues,&iCnt,&tQryTags));
+//		tag_t* tQryTags = nullptr;
+//
+//		// Smart pointer with custom deleter for query execution
+//		utils::report_error(QRY_execute(tQryTag, tEntryCnt, cEntries, cValues.get(), &iCnt, &tQryTags));
 //		tRawTags.reset(tQryTags);
-//		if(iCnt>0){
-//			printf("Query count = %d\n",iCnt);
+//		if (iCnt > 0) {
+//			printf("Query count = %d\n", iCnt);
 //		}
-//		for(int i=0;i<tEntryCnt;i++){
-//			MEM_free(cValues[i]);
-//		}
-//		MEM_free(cValues);
-//	}else{
+//
+//		// Clean up dynamically allocated strings
+//		utils::cleanUpStrings(cValues.get(),tEntryCnt);
+//
+//	} else {
 //		printf("Query not found\n");
 //	}
+//	ITK_exit_module(true);
 //	return iFail;
 //}
-
-
-//SECOND CODE	
-#include "header.h"
-#include <memory>
-#include <cstring>
-
-struct MemDeleter {
-	void operator()(tag_t* ptr) const {
-		if (ptr) MEM_free(ptr);
-	}
-};
-
-using namespace std;
-
-int ITK_user_main(int argc, char* argv[]) {
-	printf("Utility started execution...\n");
-	std::unique_ptr<tag_t, MemDeleter> tRawTags;
-	int iFail = ITK_ok;
-	iFail = ITK_auto_login();
-
-	if (iFail != ITK_ok) {
-		printf("Auto login unsuccessful\n");
-		return iFail;
-	}
-	printf("Auto login successful\n");
-	tag_t tQryTag = NULLTAG;
-	utils::report_error(QRY_find("General...", &tQryTag));
-	if (tQryTag != NULLTAG) {
-		printf("Query found\n");
-		int tEntryCnt = 1;
-		const char* cValue1 = "Item";
-		 char* cEntries[] = {"Type"};
-
-		// Use smart pointer for dynamic array allocation
-		unique_ptr<char*[]> cValues(new char*[tEntryCnt]);
-		if (!cValues) {
-			printf("Memory allocation failed for cValues\n");
-			return 1;
-		}
-
-		// Allocate memory for the first entry
-		cValues[0] = new char[strlen(cValue1) + 1];
-		if (!cValues[0]) {
-			printf("Memory allocation failed for cValues[0]\n");
-			return 2;
-		}
-
-		// Copy string safely
-		strcpy_s(cValues[0], strlen(cValue1) + 1, cValue1);
-		int iCnt = 0;
-		tag_t* tQryTags = nullptr;
-
-		// Smart pointer with custom deleter for query execution
-		utils::report_error(QRY_execute(tQryTag, tEntryCnt, cEntries, cValues.get(), &iCnt, &tQryTags));
-		tRawTags.reset(tQryTags);
-		if (iCnt > 0) {
-			printf("Query count = %d\n", iCnt);
-		}
-
-		// Clean up dynamically allocated strings
-		utils::cleanUpStrings(cValues.get(),tEntryCnt);
-
-	} else {
-		printf("Query not found\n");
-	}
-
-	return iFail;
-}
 
 
