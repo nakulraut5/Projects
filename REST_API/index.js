5b7b7c30b868a385412bdd3e3d3296fb68107e1f
nakul <nakul.raut5@gmail.com>
2025-04-27 18:03:49 +0530

    added 2 get routes

--- REST_API/index.js
+++ REST_API/index.js
@@ -1,9 +1,24 @@
 const express = require("express")
-
+const users = require("./MOCK_DATA.json")
 const app = express();
 
 const PORT = 8000;
 
 app.listen(PORT, ()=>{
     console.log(`Server is running at PORT: ${PORT}`)
+});
+
+//get users
+app.get("/api/users",(req , res)=>{
+    console.log("done");
+    return res.json(users);
+})
+
+app.get("/api/users/:id",(req,res)=>{
+    const userId = Number(req.params.id);
+    const user = users.find(u => u.id === userId);
+    if(user === null || typeof user === "undefined"){
+        res.json({ message: "No data found" });
+    }
+    return res.json(user);
 })
