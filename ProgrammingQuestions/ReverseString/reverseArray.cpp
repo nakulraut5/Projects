700ac372fa56ea3e8e21988fda2f2d925d882ec8
nakul <nakul.raut5@gmail.com>
2025-05-26 00:31:05 +0530

    files added for reverse array question

--- ProgrammingQuestions/ReverseString/reverseArray.cpp
+++ ProgrammingQuestions/ReverseString/reverseArray.cpp
@@ -1,25 +1,19 @@
-int main() {
-   char* mainStr = (char*)calloc(5,sizeof(char));
-   if(!mainStr){
-       printf("\nERROR:: Failed to allocate memory");
-       return 1;
-   }
-   for(int i=0;i<5;i++){
-       mainStr[i]='A'+i;
-   }
-   printf("Main Str: %s",mainStr);
-   char* revStr = (char*)calloc(5,sizeof(char));
-   if(!revStr){
-       printf("\nERROR:: Failed to allocate memory");
-       return 1;
-   }
-   int j=0;
-   for(int i=4;i>=0;i--){
-       revStr[j]=mainStr[i];
-       j++;
-   }
-    printf("\nReverse Str: %s",revStr);
-    free(mainStr);
-    free(revStr);
+#include <iostream>
+using namespace std;
+int main(){
+    char* mainStr = new char[5];
+    for(int i=0;i<5;i++){
+        mainStr[i]='A'+i;
+    }
+    cout << "Main Str: "<< mainStr << endl;
+    char* revStr = new char[5];
+    int j=0;
+    for(int i=4;i>=0;i--){
+        revStr[j]=mainStr[i];
+        j++;
+    }
+    cout << "Reverse Str: "<< revStr << endl;
+    delete[] mainStr;
+    delete[] revStr;
     return 0;
 }
